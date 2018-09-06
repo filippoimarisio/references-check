85ee1d0a1f5ca32b3f15a95c19890897252ea8fd
filippoimarisio <filippo.imarisio@gmail.com>
2018-09-06 17:45:39 +0200

    final trial day

--- src/components/referencesCheck.js
+++ src/components/referencesCheck.js
@@ -2,13 +2,12 @@
 export const referencesCheck = (thesis, references) => {
 
   const splitReferences = references.split("\n")
-  const splitRef = splitReferences.map(reference => reference.split(/(\,|\.)/))
-  console.log(splitRef)
-
+  const splitRef = splitReferences.map(reference => reference.split(/(\,|\.\ )/))  
   const missingReferences = [] 
 
   splitRef.map(element => {
     if (!thesis.includes(element[0])) {
+
       const missingRef = splitRef.indexOf(element)
       missingReferences.push(splitReferences[missingRef])
     }
